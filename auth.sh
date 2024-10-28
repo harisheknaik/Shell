c6b73eb15187b7cb39f189199ab75f3983f06dd6
harish <harish@gmail.com>
2024-10-28 03:33:23 +0000

    Updated auth.sh

--- auth.sh
+++ auth.sh
@@ -5,6 +5,9 @@ read pwd
 if [ $usn = "admin" -a $pwd = "testyantra123" ];
 then
 echo "Authentication Successfull"
+elif [ $usn = "Developer" -a $pwd = "Fireflink@123" ]
+then
+echo "Authentication is successfull" 
 else
 echo "Authentication Failed"
 fi

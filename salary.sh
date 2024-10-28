5f04fc63815b04c70b30fcd63aaf2fe92cdb0653
harish <harish@gmail.com>
2024-10-28 03:44:29 +0000

    Updated salary.sh

--- salary.sh
+++ salary.sh
@@ -4,6 +4,9 @@ read salary
 if [ $salary -lt 15000 ];
 then
 echo "No Tax for you"
+elif [ $salary -eq 15000 ];
+then
+echo "You need to pay 2% Tax"
 elif [ $salary -gt 15000 -a $salary -le 30000 ];
 then
 echo "You need to pay 5% Tax"

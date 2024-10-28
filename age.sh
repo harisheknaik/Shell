74cb56c5914b44da76d8be11e99082b0d0108e76
harish <harish@gmail.com>
2024-10-28 04:06:49 +0000

    Commit in First Branch

--- age.sh
+++ age.sh
@@ -6,9 +6,12 @@ echo "Child"
 elif [ $age -ge 13 -a $age -le 19 ];
 then
 echo "Teenager"
-elif [ $age -ge 20 ];
+elif [ $age -ge 20 -a $age -le 60 ];
 then
 echo "Adult"
+elif [ $age -gt 60 ];
+then
+echo "Oldage"
 else
 echo "Wrong age"
 fi
